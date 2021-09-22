ecf99e7bf4a9cbc8ef82cd9703bef2dabe2c8015
Kristy Yoshimoto <saoriyo99@yahoo.com>
2021-09-22 09:26:33 -0400

    Reload button works

--- app/public/js/aboutMe.js
+++ app/public/js/aboutMe.js
@@ -4,20 +4,30 @@ const RandomPerson = {
             "person":{}
         }
     },
-
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.person.dob.date)
+            .format('D MMM YYYY');
+        }
+    },
+    methods: {
+        fetchUserData() {
+            // creates a response
+            fetch('https://randomuser.me/api/')
+            // handles the promise
+            // parameter response, returns response.json
+            .then(response => response.json())
+            .then((parsedJSON) => {
+                console.log(parsedJSON);
+                this.person = parsedJSON.results[0];
+            })
+            .catch(err => {
+                console.error(err)
+            })
+        }
+    },
     created() {
-        // creates a response
-        fetch('https://randomuser.me/api/')
-        // handles the promise
-        // parameter response, returns response.json
-        .then(response => response.json())
-        .then((parsedJSON) => {
-            console.log(parsedJSON);
-            this.person = parsedJSON.results[0];
-        })
-        .catch(err => {
-            console.error(err)
-        })
+        this.fetchUserData();
     }
 }
 
