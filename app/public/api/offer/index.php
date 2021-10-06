20d17f9c2834c9546d336a07d910a13d570123b0
Kristy Yoshimoto <saoriyo99@yahoo.com>
2021-10-06 11:44:11 -0400

    Class notes on 10/6

--- app/public/api/offer/index.php
+++ app/public/api/offer/index.php
@@ -6,16 +6,23 @@ require 'class/DbConnection.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM student';
+$sql = 'SELECT * FROM offer';
 $vars = [];
+
+// Check if value student is not null
+if (isset($_GET['student'])) {
+  // This is an example of a parameterized query
+  $sql = 'SELECT * FROM offer WHERE studentId = ?';
+  $vars = [ $_GET['student'] ]; //array of all students
+}
 
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
-$students = $stmt->fetchAll();
+$offers = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($students, JSON_PRETTY_PRINT);
+$json = json_encode($offers, JSON_PRETTY_PRINT);
 
 // Step 4: Output
 header('Content-Type: application/json');
--- app/public/js/aboutMe.js
+++ app/public/js/aboutMe.js
@@ -1,13 +1,9 @@
 const RandomPerson = {
     data() {
         return {
-            "person": {
-                name: {},
-                dob: {},
-                picture: {},
-                location: {},
-                email: {}
-            }
+            "students": [],
+            "offers": [],
+            "selectedStudent": null
         }
     },
     computed: {
@@ -17,15 +13,46 @@ const RandomPerson = {
         }
     },
     methods: {
-        fetchUserData() {
+        prettyDollar(n) {
+            const d = new Intl.NumberFormat("en-US").format(n);
+            return "$ " + d;
+        },
+        selectStudent(s) {
+            // Don't fetch same student multiple times
+            if (s == this.selectedStudent) {
+                return;
+            }
+            // Set student
+            this.selectedStudent = s;
+            // Clear out offers array
+            this.offers = [];
+            // Get offers from student
+            this.fetchOfferData(this.selectedStudent);
+        },
+        fetchStudentData() {
             // creates a response
-            fetch('https://randomuser.me/api/')
+            fetch('/api/student/')
             // handles the promise
             // parameter response, returns response.json
             .then(response => response.json())
             .then((parsedJSON) => {
                 console.log(parsedJSON);
-                this.person = parsedJSON.results[0];
+                this.students = parsedJSON;
+            })
+            .catch(err => {
+                console.error(err)
+            })
+        },
+        fetchOfferData(s) {
+            console.log("Fetching offers for", s);
+            // creates a response
+            fetch('/api/offer/?student=' + s.id)
+            // handles the promise
+            // parameter response, returns response.json
+            .then(response => response.json())
+            .then((parsedJSON) => {
+                console.log(parsedJSON);
+                this.offers = parsedJSON;
             })
             .catch(err => {
                 console.error(err)
@@ -33,7 +60,7 @@ const RandomPerson = {
         }
     },
     created() {
-        this.fetchUserData();
+        this.fetchStudentData();
     }
 }
 
